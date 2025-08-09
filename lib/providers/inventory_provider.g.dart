bd7d6f43659bacbe88a1333acf6eaf2fde4c02d4
davidfennell <dfennell31@gmail.com>
2025-08-09 11:38:17 -0400

    Fixing cocoapods versioning again

--- lib/providers/inventory_provider.g.dart
+++ lib/providers/inventory_provider.g.dart
@@ -6,21 +6,58 @@ part of 'inventory_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+@ProviderFor(InventoryNotifier)
+const inventoryNotifierProvider = InventoryNotifierProvider._();
+
+final class InventoryNotifierProvider
+    extends $NotifierProvider<InventoryNotifier, List<InventoryItem>> {
+  const InventoryNotifierProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'inventoryNotifierProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$inventoryNotifierHash();
+
+  @$internal
+  @override
+  InventoryNotifier create() => InventoryNotifier();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(List<InventoryItem> value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<List<InventoryItem>>(value),
+    );
+  }
+}
+
 String _$inventoryNotifierHash() => r'ca054c9a73e4ff294324f825703346886e03ca20';
 
-/// See also [InventoryNotifier].
-@ProviderFor(InventoryNotifier)
-final inventoryNotifierProvider =
-    NotifierProvider<InventoryNotifier, List<InventoryItem>>.internal(
-      InventoryNotifier.new,
-      name: r'inventoryNotifierProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$inventoryNotifierHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
+abstract class _$InventoryNotifier extends $Notifier<List<InventoryItem>> {
+  List<InventoryItem> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<List<InventoryItem>, List<InventoryItem>>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<List<InventoryItem>, List<InventoryItem>>,
+              List<InventoryItem>,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
 
-typedef _$InventoryNotifier = Notifier<List<InventoryItem>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/providers/onboarding_provider.g.dart
+++ lib/providers/onboarding_provider.g.dart
@@ -6,23 +6,47 @@ part of 'onboarding_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+@ProviderFor(hasSeenOnBoardingScreen)
+const hasSeenOnBoardingScreenProvider = HasSeenOnBoardingScreenProvider._();
+
+final class HasSeenOnBoardingScreenProvider
+    extends $FunctionalProvider<bool, bool, bool>
+    with $Provider<bool> {
+  const HasSeenOnBoardingScreenProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'hasSeenOnBoardingScreenProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$hasSeenOnBoardingScreenHash();
+
+  @$internal
+  @override
+  $ProviderElement<bool> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  bool create(Ref ref) {
+    return hasSeenOnBoardingScreen(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(bool value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<bool>(value),
+    );
+  }
+}
+
 String _$hasSeenOnBoardingScreenHash() =>
     r'3097ce6a40cb295c79eb153e3a09d0d98c88bde3';
 
-/// See also [hasSeenOnBoardingScreen].
-@ProviderFor(hasSeenOnBoardingScreen)
-final hasSeenOnBoardingScreenProvider = AutoDisposeProvider<bool>.internal(
-  hasSeenOnBoardingScreen,
-  name: r'hasSeenOnBoardingScreenProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$hasSeenOnBoardingScreenHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef HasSeenOnBoardingScreenRef = AutoDisposeProviderRef<bool>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
